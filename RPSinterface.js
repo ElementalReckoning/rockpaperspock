6bdeb002dff35fc956c7b71fc981209f6c1b74b1
elementalreckoning <boogdoesit@gmail.com>
2020-11-05 15:11:51 -0800

    add RPSinterface.html .js .css

--- RPSinterface.js
+++ RPSinterface.js
@@ -2,11 +2,45 @@ let pick = [ "rock","paper","scissors"];
 let playerScore = 0;
 let computerScore = 0;
 let computerSelection = computerPlay();
+//alert("Choose Rock, Paper, or Scissors and see if you can beat the computer! Best out of five wins!!")
 
-let score = document.getElementById("score");
-score.innerText = "This is the score";
+
 let playScore = document.getElementById("playScore");
+playScore.innerText = playerScore;
 let compScore = document.getElementById("compScore");
+compScore.innerText = computerScore;
+
+
+// Get the modal
+let modal0 = document.getElementById("myModal0");
+let modal1 = document.getElementById("myModal1");
+
+// Get the button that opens the modal
+let btn = document.getElementById("rock");
+
+// Get the <span> element that closes the modal
+let span0 = document.getElementsByClassName("close0")[0];
+let span1 = document.getElementsByClassName("close0")[0];
+
+
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal0) {
+    modal0.style.display = "none";
+  }
+} 
+
+
+let spans= document.getElementsByClassName("close0");
+for (let i = 0; i < spans.length; i++) {
+    spans[i].addEventListener('click',function() {
+        modal0.style.display = "none";
+        modal1.style.display = "none";
+    }
+)};
+
+
 
 
 let rock = document.getElementById("rock");
@@ -15,14 +49,14 @@ rock.addEventListener("click", function playRound() {
     let playerSelection = "rock";
     console.log(playerSelection, computerSelection);
     if (computerSelection == "rock") {
-        alert("It's a tie!!!");
         compScore.innerText = computerScore;
         playScore.innerText = playerScore;
+        modal0.style.display = "block";
     } else if (computerSelection == "scissors") {
-        alert("You win!!! Rock beats scissors!");
         playerScore += 1;
         compScore.innerText = computerScore;
         playScore.innerText = playerScore;
+        modal1.style.display = "block";
     } else {
         alert("You lose!!! Paper beats Rock!");
         computerScore += 1;
@@ -40,6 +74,7 @@ paper.addEventListener("click", function playRound() {
         alert("It's a tie!!!");
         compScore.innerText = computerScore;
         playScore.innerText = playerScore;
+
     } else if ( computerSelection ==  "rock") {
         alert("You win!!! Paper beats Rock!");
         playerScore += 1;
@@ -57,6 +92,7 @@ let scissors = document.getElementById("scissors");
 scissors.addEventListener("click", function playRound() {
     let playScore = document.getElementById("playScore");
     let playerSelection = "scissors";
+    /*let modal = document.getElementById("myModal");*/
     console.log(playerSelection, computerSelection);
     if (computerSelection == "scissors") {
         alert("It's a tie!!!");
@@ -72,8 +108,11 @@ scissors.addEventListener("click", function playRound() {
         computerScore += 1;
         compScore.innerText = computerScore;
         playScore.innerText = playerScore;
+        modal.innerText = "Computer Wins!!!"
     }
 });
+
+
 
 function playRound(playerSelection, computerSelection) {
     playerSelection = playerSelection.toLowerCase();
@@ -92,6 +131,3 @@ function game() {
             alert(score);
     }
 };
-
-
-    //game();
