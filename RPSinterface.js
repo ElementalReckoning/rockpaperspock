3fce60e9797fe0d1e2d0a65b1f7eb2152dd90942
dentl <boogdoesit@gmail.com>
2020-11-07 10:07:36 -0800

    add final changes for turn in to TOP

--- RPSinterface.js
+++ RPSinterface.js
@@ -1,133 +1,148 @@
 let pick = [ "rock","paper","scissors"];
 let playerScore = 0;
 let computerScore = 0;
-let computerSelection = computerPlay();
+let namep = prompt("What is your name?");
 //alert("Choose Rock, Paper, or Scissors and see if you can beat the computer! Best out of five wins!!")
+
+let name = document.getElementById("div0");
+name.innerText = namep + " VS Computer";
 
 
 let playScore = document.getElementById("playScore");
 playScore.innerText = playerScore;
 let compScore = document.getElementById("compScore");
 compScore.innerText = computerScore;
+let round = document.getElementById("div1");
 
 
-// Get the modal
 let modal0 = document.getElementById("myModal0");
 let modal1 = document.getElementById("myModal1");
-
-// Get the button that opens the modal
-let btn = document.getElementById("rock");
-
-// Get the <span> element that closes the modal
-let span0 = document.getElementsByClassName("close0")[0];
-let span1 = document.getElementsByClassName("close0")[0];
-
-
+let modal2 = document.getElementById("myModal2");
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal0) {
-    modal0.style.display = "none";
-  }
-} 
+window.onclick = function(e) {
+    if (e.target == modal0 || e.target == modal1 || e.target == modal2) {
+      modal0.style.display = "none";
+      modal1.style.display = "none";
+      modal2.style.display = "none";
+    }
+  } 
 
-
-let spans= document.getElementsByClassName("close0");
+let spans = document.getElementsByClassName("close0");
 for (let i = 0; i < spans.length; i++) {
     spans[i].addEventListener('click',function() {
         modal0.style.display = "none";
         modal1.style.display = "none";
+        modal2.style.display = "none";
     }
 )};
 
 
 
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+    return computerSelection;
+}
+
 
 let rock = document.getElementById("rock");
 rock.addEventListener("click", function playRound() {
+    let computerSelection = pick[ Math.floor(Math.random() * pick.length) ];
     let playScore = document.getElementById("playScore");
     let playerSelection = "rock";
+    document.getElementById('img0').classList.add('ani');
     console.log(playerSelection, computerSelection);
     if (computerSelection == "rock") {
         compScore.innerText = computerScore;
         playScore.innerText = playerScore;
-        modal0.style.display = "block";
-    } else if (computerSelection == "scissors") {
+        div1.innerText = "It's A Tie!!!"
+    } else if (computerSelection == "scissors") { (playScore)
         playerScore += 1;
         compScore.innerText = computerScore;
         playScore.innerText = playerScore;
-        modal1.style.display = "block";
-    } else {
-        alert("You lose!!! Paper beats Rock!");
+        div1.innerText = "You Win!!!"
+    } else if (computerSelection == "paper") {
         computerScore += 1;
         compScore.innerText = computerScore;
         playScore.innerText = playerScore;
+        div1.innerText ="Computer Wins!!!"
     }
+    reset();
 });
 
 let paper = document.getElementById("paper");
-paper.addEventListener("click", function playRound() {
+paper.addEventListener("click", function game() {
+    let computerSelection = pick[ Math.floor(Math.random() * pick.length) ];
     let playScore = document.getElementById("playScore");
     let playerSelection = "paper";
     console.log(playerSelection, computerSelection);
     if (computerSelection == "paper") {
-        alert("It's a tie!!!");
         compScore.innerText = computerScore;
         playScore.innerText = playerScore;
-
+        div1.innerText = "It's A Tie!!!"
     } else if ( computerSelection ==  "rock") {
-        alert("You win!!! Paper beats Rock!");
         playerScore += 1;
         compScore.innerText = computerScore;
         playScore.innerText = playerScore;
+        div1.innerText = "You Win!!!"
     } else {
-        alert("You lose!!! Scissors beats Paper!");
         computerScore += 1;
         compScore.innerText = computerScore;
         playScore.innerText = playerScore;
+        div1.innerText ="Computer Wins!!!"
     }
+    reset();
 });
 
 let scissors = document.getElementById("scissors");
 scissors.addEventListener("click", function playRound() {
+    let computerSelection = pick[ Math.floor(Math.random() * pick.length) ];
     let playScore = document.getElementById("playScore");
     let playerSelection = "scissors";
-    /*let modal = document.getElementById("myModal");*/
     console.log(playerSelection, computerSelection);
     if (computerSelection == "scissors") {
-        alert("It's a tie!!!");
         compScore.innerText = computerScore;
         playScore.innerText = playerScore;
+        div1.innerText = "It's A Tie!!!"
     } else if (computerSelection == "paper") {
-        alert("You win!!! Scissors beats Paper!");
         playerScore += 1;
         compScore.innerText = computerScore;
         playScore.innerText = playerScore;
-    } else {
-        alert("You lose!!! Rock beats scissors!");
+        div1.innerText = "You Win!!!"
+    } else { 
         computerScore += 1;
         compScore.innerText = computerScore;
         playScore.innerText = playerScore;
-        modal.innerText = "Computer Wins!!!"
+        div1.innerText ="Computer Wins!!!"
     }
+    reset();
 });
 
 
 
-function playRound(playerSelection, computerSelection) {
-    playerSelection = playerSelection.toLowerCase();
-};
 
-//work into each separate event listener
-function computerPlay() {
-    let computerSelection = pick[ Math.floor(Math.random() * pick.length) ]
-    return computerSelection;
-}
-
-function game() {
-    while (playerScore < 5 && computerScore < 5) {
-        playRound(playerSelection, computerSelection);
-            let score = `The score is ${playerScore} to ${computerScore}`;
-            alert(score);
+function endGame() {
+    if (playerScore === 5) {
+        modal1.style.display = "block";
+      return true;
+    } else if (computerScore === 5) {
+        modal2.style.display = "block";
+        return true;
     }
-};
+    return false;
+  }
+
+  function reset() {
+    if (endGame()) {
+      setTimeout(function(){
+        playerScore = 0;
+        computerScore = 0;
+        playScore.innerText = playerScore;
+        compScore.innerText = computerScore;
+        gameActive = false;
+        round.innerText ="";
+        /*let namep = prompt("What is your name?");
+        name.innerText = namep + " VS Computer";*/
+      } )    
+    }
+  }
